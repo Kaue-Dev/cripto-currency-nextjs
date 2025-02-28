1b20b4df8e8f027c2e6fd1109276fac4c6c2d7c5
Kaue-Dev <kauemassola900@gmail.com>
2025-02-28 14:37:54 -0300

    feat/refactor: add more props and refactore code in separeted components

--- src/components/cripto-card/cripto-card.tsx
+++ src/components/cripto-card/cripto-card.tsx
@@ -1,16 +1,29 @@
+// Images
 import Image from "next/image";
+
+// Components
+import { FluctuationPercent } from "./fluctuation-percent";
+import { CriptoPrice } from "./cripto-price";
+import { CriptoName } from "./cripto-name";
 
 interface CriptoCardProps {
   id: string;
+  symbol: string;
   name: string;
   image: string;
+  current_price: number;
+  price_change_24h: number;
 }
 
 export function CriptoCard (props: CriptoCardProps) {
   return (
-    <div key={props.id} className="py-4 px-2 border border-zinc-800 bg-zinc-900 w-full max-w-56 flex flex-col items-center gap-4 rounded">
+    <div key={props.id} className="p-4 border border-zinc-800 bg-zinc-900 w-full max-w-56 flex flex-col items-center justify-center gap-4 rounded">
       <Image src={props.image} alt={props.name} width={32} height={32} />
-      <h3 className="text-lg">{props.name}</h3>
+      <CriptoName name={props.name} symbol={props.symbol} />
+      <div className="w-full flex justify-between items-center">
+        <CriptoPrice current_price={props.current_price} />
+        <FluctuationPercent currentPrice={props.current_price} priceChange24h={props.price_change_24h} />
+      </div>
     </div>
   )
 }
