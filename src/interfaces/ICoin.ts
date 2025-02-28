76ebe4ba74d27ab2b2397e2942bba75f570c6664
Kaue-Dev <kauemassola900@gmail.com>
2025-02-28 14:35:33 -0300

    feat: added symbol and price change 24h

--- src/interfaces/ICoin.ts
+++ src/interfaces/ICoin.ts
@@ -1,5 +1,6 @@
 export interface ICoin {
   id: string;
+  symbol: string;
   name: string;
   image: string;
   current_price: number;
@@ -7,5 +8,6 @@ export interface ICoin {
   market_cap_rank: number;
   high_24h: number;
   low_24h: number;
+  price_change_24h: number;
   last_updated: string;
 }
