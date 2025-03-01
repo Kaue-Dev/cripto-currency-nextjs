b5174bcb3f7077ebdef9a9cc7352bec90d7dca86
Kaue-Dev <kauemassola900@gmail.com>
2025-03-01 13:06:57 -0300

    refactor: remove search bar and crypto list and turn in component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,23 @@
-// Icons
-import { Search } from "lucide-react";
-
-// Images
 import Image from "next/image";
-import Logo from "@/assets/images/logo.png"
+import Logo from "@/assets/images/logo.png";
+import { Content } from "@/components/content/content";
+import { fetchCoinsData } from "@/domain/services/fetchCoinsData";
 
-// Components
-import { CriptoList } from "@/components/cripto-list/cripto-list";
+export default async function Page() {
 
-export default function Page() {
+  const data = await fetchCoinsData()
+
   return (
-    <div className="max-w-7xl mx-auto py-8">
-      <Image src={Logo} alt="Crypto Currency" width={300} height={200} className="invert mb-8 select-none" />
+    <div className="w-full max-w-7xl mx-auto py-8">
+      <Image
+        src={Logo}
+        alt="Crypto Currency"
+        width={300}
+        height={200}
+        className="invert mb-8 select-none"
+      />
 
-      <div className="mb-8 py-2 px-4 bg-zinc-900 border border-zinc-800 flex items-center rounded-full w-full max-w-96">
-        <input 
-          type="text" 
-          className="w-full outline-0 text-base placeholder:text-zinc-600"
-          placeholder="Search for a cryptocurrency"
-        />
-        <Search size={20} className="text-zinc-600" />
-      </div>
-
-      <CriptoList />
+      <Content data={data} />
     </div>
-  )
+  );
 }
