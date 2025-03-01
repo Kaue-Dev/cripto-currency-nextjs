3bb0a0a0058b24bfdbcdd779109b6cbfb4093d69
Kaue-Dev <kauemassola900@gmail.com>
2025-03-01 13:25:39 -0300

    feat: add filter by name and symbol and searchValue prop in props list

--- src/components/cripto-list/cripto-list.tsx
+++ src/components/cripto-list/cripto-list.tsx
@@ -3,14 +3,17 @@ import { TCoin } from "@/entities/TCoin";
 
 interface CriptoListProps {
   data: Array<TCoin>;
+  searchValue: string;
 }
 
-export function CriptoList({data}: CriptoListProps) {
+export function CriptoList({ data, searchValue }: CriptoListProps) {
   return (
     <div className="flex flex-wrap gap-8 justify-start items-start">
       {data.length <= 0 && <span>No cryptocurrencies found.</span>}
-      {data.length > 0 &&
-        data.map((coin) => {
+
+      {data.length > 0 && data
+        .filter((coin) => coin.name.toLowerCase().includes(searchValue.toLowerCase()) || coin.symbol.toLowerCase().includes(searchValue.toLowerCase()))
+        .map((coin) => {
           return (
             <CriptoCard
               key={coin.id}
