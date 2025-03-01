a771c02b9ca8add2c8ee97da26fbc18f601c9bc5
Kaue-Dev <kauemassola900@gmail.com>
2025-03-01 13:06:10 -0300

    refactor: removed coins fetch

--- src/components/cripto-list/cripto-list.tsx
+++ src/components/cripto-list/cripto-list.tsx
@@ -1,26 +1,11 @@
-"use server";
-
-// Interfaces
-import { ICoin } from "@/interfaces/ICoin";
-
-// Components
 import { CriptoCard } from "@/components/cripto-card/cripto-card";
+import { TCoin } from "@/entities/TCoin";
 
-export async function CriptoList() {
+interface CriptoListProps {
+  data: Array<TCoin>;
+}
 
-  const baseUrl = process.env.API_BASE_URL;
-  const apiKey = process.env.API_KEY;
-
-  const coinsID = "bitcoin,ethereum,tether,binancecoin,usd-coin,ripple,cardano,solana,dogecoin,polkadot,dai,shiba-inu,tron,uniswap,avalanche-2";
-
-  const response = await fetch(`${baseUrl}coins/markets?vs_currency=usd&ids=${coinsID}&x_cg_demo_api_key=${apiKey}`);
-
-  if (!response.ok) {
-    throw new Error("Failed to fetch data from API");
-  }
-
-  const data: ICoin[] = await response.json();
-  
+export function CriptoList({data}: CriptoListProps) {
   return (
     <div className="flex flex-wrap gap-8 justify-start items-start">
       {data.length <= 0 && <span>No cryptocurrencies found.</span>}
