e33f4146d59f312cecabb980679c8f0de0c1f8d5
Kaue-Dev <kauemassola900@gmail.com>
2025-03-06 15:05:30 -0300

    feat: add props

--- src/components/sort-section/sort-option.tsx
+++ src/components/sort-section/sort-option.tsx
@@ -2,9 +2,12 @@ interface SortOptionProps extends React.HTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
 }
 
-export function SortOption({ children }: SortOptionProps) {
+export function SortOption({ children, ...props }: SortOptionProps) {
   return (
-    <button className="w-full py-2 px-4 rounded cursor-pointer text-start hover:bg-zinc-800">
+    <button 
+      className="flex items-center justify-between w-full py-2 px-4 rounded cursor-pointer text-start hover:bg-zinc-800"
+      {...props}
+    >
       {children}
     </button>
   );
