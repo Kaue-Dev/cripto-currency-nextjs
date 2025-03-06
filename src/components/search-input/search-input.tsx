476b1f2ee7444dff50b38246b28001814827ac2c
Kaue-Dev <kauemassola900@gmail.com>
2025-03-06 12:41:05 -0300

    feat: created a search input component

---
+++ src/components/search-input/search-input.tsx
@@ -0,0 +1,24 @@
+import { Search } from "lucide-react";
+
+interface SearchInputProps {
+  searchValue: string;
+  setSearchValue: (value: string) => void;
+}
+
+export function SearchInput({
+  searchValue,
+  setSearchValue,
+}: SearchInputProps) {
+  return (
+    <div className="py-2 px-4 bg-zinc-900 border border-zinc-800 flex items-center rounded w-full max-w-96">
+      <input
+        type="text"
+        className="w-full outline-0 text-base placeholder:text-zinc-600"
+        placeholder="Search for a cryptocurrency"
+        value={searchValue}
+        onChange={(e) => setSearchValue(e.target.value)}
+      />
+      <Search size={20} className="text-zinc-600" />
+    </div>
+  );
+}
