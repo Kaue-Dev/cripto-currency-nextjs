f3fcbc3194426cbe82741540c4e98fb0ff3ea90d
Kaue-Dev <kauemassola900@gmail.com>
2025-03-07 14:19:18 -0300

    refactor: add new class to style background color and border color

--- src/components/change-layout/change-layout.tsx
+++ src/components/change-layout/change-layout.tsx
@@ -8,7 +8,7 @@ interface ChangeLayoutProps {
 export function ChangeLayout({ handleChangeLayout, gridLayout }: ChangeLayoutProps) {
   return (
     <button
-      className="p-2.5 bg-zinc-900 border border-zinc-800 rounded cursor-pointer text-zinc-600 hover:bg-zinc-800"
+      className="p-2.5 dark-bg-border rounded cursor-pointer text-zinc-600 hover:bg-zinc-800"
       onClick={handleChangeLayout}
     >
       {gridLayout ? <LayoutGrid size={20} /> : <LayoutList size={20} />}
