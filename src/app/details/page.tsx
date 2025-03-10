6949887c1eeba2be7152fdb543b512209242e980
Kaue-Dev <kauemassola900@gmail.com>
2025-03-10 15:38:25 -0300

    feat: add texts and home button

--- src/app/details/page.tsx
+++ src/app/details/page.tsx
@@ -1,9 +1,26 @@
+//import { fetchCoinsData } from "@/domain/services/fetchCoinsData";
+import Link from "next/link";
 import { ChartComponent } from "./components/chart-component";
+import { Home } from "lucide-react";
 
-export default function Details() {
+export default async function Details() {
+
+  //const data = await fetchCoinsData()
+
   return (
-    <div className="w-full max-w-3xl mx-auto">
-      <ChartComponent />
+    <div className="w-full max-w-7xl mx-auto py-8 px-4">
+      <div className="mb-8 flex justify-between items-end gap-4">
+        <div>
+          <h1 className="text-4xl font-bold">Details</h1>
+          <p>Select the cryptocurrency you want more information about.</p>
+        </div>
+        <Link href="/" className="bg-zinc-900 border border-zinc-800 p-3 text-zinc-400 rounded w-fit hover:bg-zinc-800">
+          <Home size={24} />
+        </Link>
+      </div>
+      <div className="w-full">
+        <ChartComponent />
+      </div>
     </div>
   )
 }
