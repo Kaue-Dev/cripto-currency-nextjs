19ad5f67e2093b40d79ef8e06e4bbeb04ef6aa43
Kaue-Dev <kauemassola900@gmail.com>
2025-03-14 19:38:13 -0300

    refactor: remove chart component

--- src/app/details/page.tsx
+++ src/app/details/page.tsx
@@ -1,12 +1,7 @@
-//import { fetchCoinsData } from "@/domain/services/fetchCoinsData";
 import Link from "next/link";
-import { ChartComponent } from "./components/chart-component";
 import { Home } from "lucide-react";
 
 export default async function Details() {
-
-  //const data = await fetchCoinsData()
-
   return (
     <div className="w-full max-w-7xl mx-auto py-8 px-4">
       <div className="mb-8 flex justify-between items-end gap-4">
@@ -17,9 +12,6 @@ export default async function Details() {
         <Link href="/" className="bg-zinc-900 border border-zinc-800 p-3 text-zinc-400 rounded w-fit hover:bg-zinc-800">
           <Home size={24} />
         </Link>
-      </div>
-      <div className="w-full">
-        <ChartComponent />
       </div>
     </div>
   )
